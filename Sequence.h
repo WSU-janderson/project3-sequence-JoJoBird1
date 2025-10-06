268c9e3123f4a292040be30d085570d9f0482c5d
Jordan Wilson <wilson.1114@wright.edu>
2025-10-06 18:57:55 -0400

    putting outline of sequenceNode in the h file

--- Sequence.h
+++ Sequence.h
@@ -4,8 +4,8 @@
  * 
  */
 
-#ifndef CLASSES_SEQUENCE
-#define CLASSES_SEQUENCE
+#ifndef CLASSES_SEQUENCE_H
+#define CLASSES_SEQUENCE_H
 
 
 class Sequence {
@@ -20,4 +20,27 @@ public:
 //  parameterized constructor
 Sequence(size_t headPoint, size_t tailPoint);
 
+};
+
+/**
+ * Class: SequenceNode
+ * 
+ */
+class SequenceNode {
+    // public variables
+    public: 
+    SequenceNode* next; // pointer to the next Node. If node is the tail, next is nullptr
+
+    SequenceNode* prev; // point to the previous Node. If node is the head, prev is nullptr
+
+    std:: string item; // the element being stored in the node
+
+    // default constructor, ensure next and prev are nullptr
+    SequenceNode() : next(nullptr), prev(nullptr) {}
+
+    // parameterized constructor, next and prev are set to nullptr and 
+    // the node's element  is set to the given value
+    SequenceNode(std::string item) : next(nullptr), prev(nullptr), item(item) {
+
+    }
 };
