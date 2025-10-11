d8ff44a363fdb32a57cdc1d8f49556e572a87388
Jordan Wilson <wilson.1114@wright.edu>
2025-10-11 14:35:30 -0400

    added comments int sequencedebug

--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -1,7 +1,6 @@
 /**
- * SequenceDebug.cpp
- * Project 3
- * CS 3100
+ * Project 3: Linked Sequence Data Structure
+ * Name: Jordan Wilson
  *
  * In this file, you will write your tests as you implement Sequence. If you are using CLion, you need to select
  * SequenceDebug from the drop-down menu next to the Build (hammer icon) if it is on SequenceTestHarness
