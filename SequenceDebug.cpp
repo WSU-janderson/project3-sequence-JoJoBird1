bfa1ccfc19fcfd793490dff22c22f13e338d4783
Jordan Wilson <wilson.1114@wright.edu>
2025-10-14 19:09:02 -0400

    made tests for insert, front, back, size, clear, empty

--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int main() {
     Sequence s;
+    Sequence t;
 
     cout << "Test for Push Back Items: " << endl;
     s.push_back("Apples");
@@ -23,14 +24,103 @@ int main() {
     s.push_back("Fish");
     s.push_back("Grapes");
     s.push_back("Ham");
-
-    cout << "Tested Sequence: " << s << endl;
+    t.push_back("Apes");
+    t.push_back("Bonobos");
+    t.push_back("Camels");
+    t.push_back("Eagles");
+    t.push_back("Fish");
+    t.push_back("Giraffes");
+    cout << "Tested Sequence: s " << s << endl;
+    cout << "Tested Sequence: t " << t << endl;
+    cout << endl;
 
     cout << "Test for Pop Back Items: " << endl;
     s.pop_back();
     s.pop_back();
-
     cout << "Tested Sequence: " << s << endl;
+    cout << endl;
+
+    cout << "Test for Insert Items: " << endl;
+    s.insert(3,"Crackers");
+    cout << "Tested Sequence: " << s << endl;
+    cout << endl;
+
+    cout << "Test for First Item: " << endl;
+    cout << "Tested Sequence: " << s.front() << endl;
+    cout << endl;
+
+    cout << "Test for Last Item: " << endl;
+    cout << "Tested Sequence: " << s.back() << endl;
+    cout << endl;
+
+    cout << "Test for Size: " << endl;
+    cout << "Tested Sequence: " << s.size() << endl;
+    cout << endl;
+
+    cout << "Test for Erase: " << endl;
+    s.erase(3, 2);
+    cout << "Tested Sequence: " << s << endl;
+    cout << endl;
+
+    cout << "Test for Clear: " << endl;
+    s.clear();
+    cout << "Tested Sequence: " << s << endl;
+    cout << endl;
+
+    cout << "Test for Empty: " << endl;
+    cout << "Tested Sequence: " << s.empty() << endl;
+    cout << endl;
+
+    cout << "Test for Empty Front: " << endl;
+    try {
+        cout << s.front() << endl;
+    }
+    catch (out_of_range e) {
+        cout << e.what() << endl;
+    }
+    cout << endl;
+
+    cout << "Test for Empty Back: " << endl;
+    try {
+        cout << s.back() << endl;
+    }
+    catch (out_of_range e) {
+        cout << e.what() << endl;
+    }
+    cout << endl;
+
+    cout << "Test for Empty Pop Back: " << endl;
+    try {
+        s.pop_back();
+    }
+    catch (out_of_range e) {
+        cout << e.what() << endl;
+    }
+    cout << endl;
+
+
+    cout << "Test for Empty Push Back: " << endl;
+    s.push_back("Purple");
+    cout << "Tested Sequence: " << s << endl;
+    cout << endl;
+
+    cout << "Test for Invalid Insert: " << endl;
+    try {
+        s.insert(3, "No");
+    }
+    catch (out_of_range e) {
+        cout << e.what() << endl;
+    }
+    cout << endl;
+
+    cout << "Test for Invalid Erase: " << endl;
+    try {
+        s.erase(3);
+    }
+    catch (out_of_range e) {
+        cout << e.what() << endl;
+    }
+    cout << endl;
 
     return 0;
 }
