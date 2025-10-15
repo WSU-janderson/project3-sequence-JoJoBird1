6f8324e1f26250853159bf1fcd30a7ab0310111e
Jordan Wilson <wilson.1114@wright.edu>
2025-10-14 20:08:25 -0400

    comments added to sequence.h file

--- Sequence.h
+++ Sequence.h
@@ -14,14 +14,15 @@
 
 /**
  * Class: SequenceNode
- * 
+ * Class for the nodes since Sequence is a doubly-linked class. Two pointers for each node
+ * (one pointing to the next node in Sequence and the other pointing to the previous node in Sequence)
  */
 class SequenceNode {
     // public variables
     public: 
-    SequenceNode* next; // pointer to the next Node. If node is the tail, next is nullptr
-    SequenceNode* prev; // point to the previous Node. If node is the head, prev is nullptr
-    std::string item; // the element being stored in the node
+    SequenceNode* next;             // pointer to the next Node. If node is the tail, next is nullptr
+    SequenceNode* prev;             // point to the previous Node. If node is the head, prev is nullptr
+    std::string item;               // the element being stored in the node
 
     // default constructor, ensure next and prev are nullptr
     SequenceNode() : next(nullptr), prev(nullptr) {}
@@ -40,35 +41,34 @@ class SequenceNode {
 class Sequence {
 // private variables
 private: 
-size_t numElts;
-SequenceNode* headPointer;
-SequenceNode* tailPointer;
+size_t numElts;                     // variable to keep track of how many nodes are currently in linked list
+SequenceNode* headPointer;          // variable to point to the first node
+SequenceNode* tailPointer;          // variable to point to the last node
 
 // public variables 
 public:
 
 // constructors
-Sequence(size_t sz = 0); // constructor
-Sequence(const Sequence& s); // copy constructor
-~Sequence(); // de-constructor
-Sequence& operator=(const Sequence& s); // copy assignment operator
+Sequence(size_t sz = 0);                        // constructor
+Sequence(const Sequence& s);                    // copy constructor
+~Sequence();                                    // de-constructor
+Sequence& operator=(const Sequence& s);         // copy assignment operator
 
-std::string& operator[](size_t position); // access to non-const items
-//const std::string& operator[](size_t position) const // access to const items
+std::string& operator[](size_t position);       // access to non-const items
 
 // getters
-std::string front() const; // return first item in the sequence, if empty throw exception
-std::string back() const; // return the last item in the sequence, if empty throw exception
-bool empty() const; // return true if the sequence has no items
-size_t size() const; //return the items in the sequence
+std::string front() const;                      // return first item in the sequence, if empty throw exception
+std::string back() const;                       // return the last item in the sequence, if empty throw exception
+bool empty() const;                             // return true if the sequence has no items
+size_t size() const;                            //return the items in the sequence
 
 // list void methods in Sequence.cpp
-void push_back(std::string item); // item is appended to the sequence
-void pop_back(); // item at the end is deleted
+void push_back(std::string item);               // item is appended to the sequence
+void pop_back();                                // item at the end is deleted
 void insert(size_t position, std:: string item); // if position satisfies, item is inserted at position
-void clear(); // all items in sequence are deleted and memory associated is released, reset to empty
-void erase(size_t position); // item removed and memory release, if called with invalid position throws an exception
-void erase(size_t position, size_t count); // items in sequence are deleted and memory released, throws exception if invalid
+void clear();                                    // all items in sequence are deleted and memory associated is released, reset to empty
+void erase(size_t position);                    // item removed and memory release, if called with invalid position throws an exception
+void erase(size_t position, size_t count);      // items in sequence are deleted and memory released, throws exception if invalid
 
 // Friend Function. Allows access to private data. Prints items as a string to output
 friend std::ostream& operator<<(std::ostream& os, const Sequence& s);
